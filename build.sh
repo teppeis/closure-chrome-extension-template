7b29af02e32fb9202e382710d642aadbfab043fc
teppeis <teppeis@gmail.com>
2012-05-13 20:54:37 +0900

    Convert hg to git.

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 # Abort if any individual command fails.
 set -e
 
-# Ensure this is run from the root directory.
-cd `hg root`
+# Get the base directory.
+BASE_DIR=$(cd $(dirname $0);pwd)
 
 # Clean out the previous build.
-BUILD_DIR=build
+BUILD_DIR=${BASE_DIR}/build
 rm -rf ${BUILD_DIR}
 
 # Create build directories.
@@ -16,25 +16,27 @@ GOOGLE_CHROME_BUILD_DIR=${BUILD_DIR}/google-chrome
 mkdir -p ${GOOGLE_CHROME_BUILD_DIR}
 
 # Add static content
-cp -r google-chrome/* ${GOOGLE_CHROME_BUILD_DIR}
+CHROME_DIR=${BASE_DIR}/google-chrome
+cp -r ${CHROME_DIR}/* ${GOOGLE_CHROME_BUILD_DIR}
 
 # Scrub "permissions" section from manifest.json.
-sed -i -e '/"permissions"/d' ${GOOGLE_CHROME_BUILD_DIR}/manifest.json
+sed -i '' -e '/"permissions"/d' ${GOOGLE_CHROME_BUILD_DIR}/manifest.json
 
 # Compile JavaScript
-java -jar js/plovr.jar build js/chrome-contentscript-config.js > \
+JS_DIR=${BASE_DIR}/js
+java -jar ${JS_DIR}/plovr.jar build ${JS_DIR}/chrome-contentscript-config.js > \
     ${GOOGLE_CHROME_BUILD_DIR}/contentscript.js
-java -jar js/plovr.jar build js/chrome-options-config.js > \
+java -jar ${JS_DIR}/plovr.jar build ${JS_DIR}/chrome-options-config.js > \
     ${GOOGLE_CHROME_BUILD_DIR}/options.js
-java -jar js/plovr.jar build js/chrome-background-config.js > \
+java -jar ${JS_DIR}/plovr.jar build ${JS_DIR}/chrome-background-config.js > \
     ${GOOGLE_CHROME_BUILD_DIR}/background.js
 
 # Substitute <script> tags to plovr with <script> tags to packaged JavaScript.
-cp google-chrome/background.html ${GOOGLE_CHROME_BUILD_DIR}
-sed -i -e 's/<script.*script>/<script src="background.js"><\/script>/g' \
+cp ${CHROME_DIR}/background.html ${GOOGLE_CHROME_BUILD_DIR}
+sed -i '' -e 's/<script.*script>/<script src="background.js"><\/script>/g' \
     ${GOOGLE_CHROME_BUILD_DIR}/background.html
-cp google-chrome/options.html ${GOOGLE_CHROME_BUILD_DIR}
-sed -i -e 's/<script.*script>/<script src="options.js"><\/script>/g' \
+cp ${CHROME_DIR}/options.html ${GOOGLE_CHROME_BUILD_DIR}
+sed -i '' -e 's/<script.*script>/<script src="options.js"><\/script>/g' \
     ${GOOGLE_CHROME_BUILD_DIR}/options.html
 
 # Package the Chrome extension as a ZIP file.
--- serve-plovr.sh
+++ serve-plovr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cd `hg root`
+BASE_DIR=$(cd $(dirname $0);pwd)
+JS_DIR=${BASE_DIR}/js
 
 PORT=2012
 
-java -jar js/plovr.jar serve --port ${PORT} \
-    js/chrome-contentscript-config.js \
-    js/chrome-background-config.js \
-    js/chrome-options-config.js
+java -jar ${JS_DIR}/plovr.jar serve --port ${PORT} \
+    ${JS_DIR}/chrome-contentscript-config.js \
+    ${JS_DIR}/chrome-background-config.js \
+    ${JS_DIR}/chrome-options-config.js
